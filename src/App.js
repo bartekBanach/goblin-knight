7392e101e1ad2866426ba983dbda1e294eeb8002
Bartłomiej Banach <bartek.banach.47@gmail.com>
2023-11-05 19:21:28 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React from 'react'
+import Pico8 from 'react-pico-8'
+import { Controls,
+         Reset,
+         Pause,
+         Sound,
+         Carts,
+         Fullscreen } from 'react-pico-8/buttons'
+import 'react-pico-8/styles.css'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const App = () => (
+
+  <div className='App'>
+
+
+    <div className='container'>
+      <div className='logo'>
+        <h1>Goblin Knight</h1>
+        <h2>a pico-8 roguelike</h2>
+
+      </div>
+
+      <div className='game-container'>
+        <Pico8 className='game' src="roguelike.js"
+              autoPlay={true}
+              legacyButtons={false}
+              hideCursor={false}
+              center={true}
+              blockKeys={true}
+              usePointer={true}
+              unpauseOnReset={true}
         >
-          Learn React
-        </a>
-      </header>
+          <Reset/>
+          <Sound/>
+          <Fullscreen/>
+        </Pico8>
+      </div>
+
+      <div className='controls'>
+        <h2>controls</h2>
+        <p>* arrow keys - move character / navigate equipment menu</p>
+        <p>* z key - skip turn / close menu</p>
+        <p>* x key - open equipment / confirm</p>
+      </div>
     </div>
-  );
-}
+    
+  </div>
+)
 
 export default App;
